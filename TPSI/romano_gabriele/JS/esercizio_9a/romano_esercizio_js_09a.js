b343832e9dd90c52d87acec74f4b16d1465b490d
Romano-Gabriele <gabriele.romano@istitutoagnelli.it>
2024-10-04 11:48:10 +0200

    - aggiunta esercizi 01a, 01b, 03

--- TPSI/romano_gabriele/JS/esercizio_9a/romano_esercizio_js_09a.js
+++
@@ -1,7 +0,0 @@
-let utente = {
-    nome: "Mario",
-    cognome: "Rossi"
-};
-
-for(let i = 0; i < Object.keys(utente).length; i++)
-    console.log("La chiave", Object.keys(utente)[i],  "ha valore", Object.values(utente)[i]);
