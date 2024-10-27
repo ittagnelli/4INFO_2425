0a1de20576e195f90a357a37747d494aa395b0c0
Mariotti-Matteo <matteo.mariotti@istitutoagnelli.it>
2024-10-27 18:36:57 +0100

    es 08

--- TPSI/mariotti_matteo/JS/Esercizi/mariotti_esercizio_js_08/mariotti_esercizio_js_08.js
+++ TPSI/mariotti_matteo/JS/Esercizi/mariotti_esercizio_js_08/mariotti_esercizio_js_08.js
@@ -4,7 +4,7 @@ function getTaskValue(){
 }
 
 function getPriorityValue(){
-    priority = document.getElementById("priority").id;
+    priority = document.getElementById("priority").value;
     return priority;
 }
 
@@ -20,11 +20,15 @@ function aggiungi(){
     }else{
 
         const table = document.getElementById("table");
+
+
         const tr = document.createElement("td");
         const td_button = document.createElement("td");
         const td_task = document.createElement("td");
         const td_priority = document.createElement("td");
         const button = document.createElement("button");
+
+
 
         // if(getTaskValue() == "Spengere il pc"){
         //     alert("Premi ctrl + alt + f4");
@@ -33,12 +37,15 @@ function aggiungi(){
 
         td_task.innerText = getTaskValue();
         button.innerText = "Fatto"
-        if(getPriorityValue == "low"){
+
+        const priorityValue = getPriorityValue(); // Qui salviamo il valore della priorità
+
+        if(priorityValue == "low"){
             console.log("Priority value low");
             td_task.innerText = "bassa priorità";
         }
         
-        else if(getPriorityValue == "mid"){
+        else if(priorityValue    == "mid"){
             console.log("Priority value mid");
             td_task.innerText = "media priorità";
         }
@@ -53,7 +60,20 @@ function aggiungi(){
         tr.append(td_priority);
         tr.append(td_button);
         td_button.append(button);
+        td_button.innerText = "Fatto";
+        td_button.addEventListener("click",function(){rimuovi(tr)})
 
+
+        // td_button.innerText = "Rimuovi";
+        // td_button.addEventListener("click", function(){
+        //     rimuovi(tr1);
         
+
     }
+}
+
+
+function rimuovi(tr){
+    const tabella = document.getElementById("tab"); // Prendo la tabella totale
+    tabella.removeChild(tr1); // Rimuovo il child
 }
