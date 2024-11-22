42a4b13ca9946d7d4e9b70aa42497230549b404f
Trovato-Nickolas <154589467+Trovato-Nickolas@users.noreply.github.com>
2024-11-22 13:49:15 +0100

    Delete TPSI/trovato_nickolas/js/trovato_esercizio_09 directory

--- TPSI/trovato_nickolas/js/trovato_esercizio_09/trovato_esercizio_js_09.js
+++
@@ -1,4 +0,0 @@
-function login(){
-    let user = prompt('inserire nome : ','admin') || alert('Cancellato')
-    let passworld = (user == 'admin' ||  alert('Utente Errato') ) && (!(!((prompt('Inserire Passworld : ','Agnell1no') || alert('canceled')) == 'Agnell1no') || alert('benvenuto')) || alert('password sbaglaita'))
-}
