8263173be88245aef1762faafb23bc21b4d3746b
Trovato-Nickolas <154589467+Trovato-Nickolas@users.noreply.github.com>
2024-11-22 13:49:31 +0100

    s

---
+++ TPSI/trovato_nickolas/js/trovato_esercizio_09/trovato_esercizio_js_09.js
@@ -0,0 +1,4 @@
+function login(){
+    let user = prompt('inserire nome : ','admin') || alert('Cancellato')
+    let passworld = (user == 'admin' ||  alert('Utente Errato') ) && (!(!((prompt('Inserire Passworld : ','Agnell1no') || alert('canceled')) == 'Agnell1no') || alert('benvenuto')) || alert('password sbaglaita'))
+}
