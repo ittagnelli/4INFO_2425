8022fdf47ce1b0623af150d7249ff09e7c02f50b
Mariotti-Matteo <matteo.mariotti@istitutoagnelli.it>
2024-12-06 12:55:41 +0100

    es 10

--- TPSI/mariotti_matteo/JS/Esercizi/mariotti_esercizio_js_10/mariotti_esercizio_js_10.js
+++ TPSI/mariotti_matteo/JS/Esercizi/mariotti_esercizio_js_10/mariotti_esercizio_js_10.js
@@ -1,11 +1,10 @@
-function esercizio(){
+
     let a = {
-        nome: "Mario",
-        cognome: "Rossi"
+        "nome": "Mario",
+        "cognome": "Rossi"
     }
 
     x = Object.keys(a)
     for (let i = 0; i < x.lenght; i++) {
           console.log(x[i])
     }
-}
