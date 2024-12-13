f5e55a756fb45eda28ea4dd9d2c8efbd888df6ff
Mariotti-Matteo <matteo.mariotti@istitutoagnelli.it>
2024-12-13 12:56:02 +0100

    Esercizio 16 e fix 15

--- TPSI/mariotti_matteo/JS/Esercizi/mariotti_esercizio_js_16/mariotti_esercizio_js_16.js
+++ TPSI/mariotti_matteo/JS/Esercizi/mariotti_esercizio_js_16/mariotti_esercizio_js_16.js
@@ -1,35 +1,38 @@
-const orari = {
-    orario1: {h: 1, m: 26, s: 30},
-    orario2: {h: 1, m: 26, s: 20},
-    orario3: {h: 1, m: 26, s: 30}
-}
-
-const orario1 = {
-    h: 1,
-    m: 26,
-    s: 30
-}
-
-const orario2 = {
-    h: 1,
-    m: 41,
-    s: 20
-}
-
-const orario3 = {
-    h: 2,
-    m: 1,
-    s: 30
-}
-
 function orario_minore(time, time1,time2){
-const tempo1 = time.h *3600 + time.m * 60 + time.s
-const tempo2 = time1.h * 3600 + time.m * 60 + time.s
-const tempo3 = time2.h * 3600 + time.m * 60 + time.s 
+const tempo1 = time.h * 3600 + time.m * 60 + time.s
+const tempo2 = time1.h * 3600 + time1.m * 60 + time1.s
+const tempo3 = time2.h * 3600 + time2.m * 60 + time2.s 
+console.log(tempo1, tempo2, tempo3)
 
+minimo = Math.min(tempo1, tempo2, tempo3)
+console.log(minimo)
+// Math.max(time, time1, time2)
+
+console.log("Valore minore: ", )
+
+
+if(tempo1 == minimo){
+    console.log(time)
+}
+else if (tempo2 == minimo) {
+    console.log(time1)
+}
+else if (tempo3 == minimo) {
+    console.log(time2)
+}
+else{
+    console.log("Errore")
+}
 }
 
 function main(){
+    const orari = { // Ho messo un oggetto dentro un oggetto
+        orario1: {h: 1, m: 26, s: 30},
+        orario2: {h: 1, m: 30, s: 20},
+        orario3: {h: 0, m: 12, s: 30}
+    }
+    
+    orario_minore(orari.orario1, orari.orario2, orari.orario3) // Chiamo funzione orario minore
 
 }
 
