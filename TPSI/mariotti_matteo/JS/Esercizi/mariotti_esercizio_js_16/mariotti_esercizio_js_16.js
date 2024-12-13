2e87006ab7c5c4ebc4bf8fffd57d7233f17dea09
Mariotti-Matteo <matteo.mariotti@istitutoagnelli.it>
2024-12-13 12:17:29 +0100

    Es 15

---
+++ TPSI/mariotti_matteo/JS/Esercizi/mariotti_esercizio_js_16/mariotti_esercizio_js_16.js
@@ -0,0 +1,36 @@
+const orari = {
+    orario1: {h: 1, m: 26, s: 30},
+    orario2: {h: 1, m: 26, s: 20},
+    orario3: {h: 1, m: 26, s: 30}
+}
+
+const orario1 = {
+    h: 1,
+    m: 26,
+    s: 30
+}
+
+const orario2 = {
+    h: 1,
+    m: 41,
+    s: 20
+}
+
+const orario3 = {
+    h: 2,
+    m: 1,
+    s: 30
+}
+
+function orario_minore(time, time1,time2){
+const tempo1 = time.h *3600 + time.m * 60 + time.s
+const tempo2 = time1.h * 3600 + time.m * 60 + time.s
+const tempo3 = time2.h * 3600 + time.m * 60 + time.s 
+
+}
+
+function main(){
+
+}
+
+main()
