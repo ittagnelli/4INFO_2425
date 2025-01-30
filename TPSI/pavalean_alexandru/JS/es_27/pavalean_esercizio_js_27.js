ae144200731e18089c4a7bfe8674c46a2bc6fd01
Pavalean-Alexandru <alexandru.pavalean@istitutoagnelli.it>
2025-01-30 15:33:13 +0100

    consegna esercizio javascript dal 23 al 27

---
+++ TPSI/pavalean_alexandru/JS/es_27/pavalean_esercizio_js_27.js
@@ -0,0 +1,10 @@
+function capitalize_all(stringa){
+    stringa_finale = stringa.toUpperCase()
+    return stringa_finale
+}
+
+function main(){
+    console.log(capitalize_all('ciao mondo la terra gira!!!'))
+}
+
+main()
