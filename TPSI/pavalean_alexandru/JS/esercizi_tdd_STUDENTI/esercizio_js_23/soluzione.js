6b40d3259dde3ad19fa6d04ee6fa879b5aebb1ab
Pavalean-Alexandru <alexandru.pavalean@istitutoagnelli.it>
2025-01-31 13:42:36 +0100

    consegna esercizi dal 23 al 27

--- TPSI/pavalean_alexandru/JS/esercizi_tdd_STUDENTI/esercizio_js_23/soluzione.js
+++ TPSI/pavalean_alexandru/JS/esercizi_tdd_STUDENTI/esercizio_js_23/soluzione.js
@@ -1,4 +1,4 @@
 export const abbreviazione = (str) => {
-    //INSERISCI QUI IL TUO CODICE
+        return str.split(' ')[0][0].toUpperCase() + str.split(' ')[0].slice(1) + ' ' + str.split(' ')[1][0].toUpperCase() + '.'
 }
 
