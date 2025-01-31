d22e495150d35568b547576c2247f9c65932244b
Pavalean-Alexandru <alexandru.pavalean@istitutoagnelli.it>
2025-01-31 11:36:50 +0100

    consegna esercizi dal 23 al 27 javascript

---
+++ TPSI/pavalean_alexandru/JS/esercizi_tdd_STUDENTI/esercizio_js_26/soluzione.js
@@ -0,0 +1,3 @@
+export const capitalize = (str) => {
+    eturn str != '' ? str[0].toUpperCase() + str.slice(1) : str;
+}
---
+++ TPSI/pavalean_alexandru/JS/esercizi_tdd_STUDENTI/esercizio_js_27/soluzione.js
@@ -0,0 +1,6 @@
+export const capitalize_all = (str) => {
+      return str.split(' ').map(word => {
+         return word.charAt(0).toUpperCase() + word.slice(1);
+   }).join(' '); 
+}
+
