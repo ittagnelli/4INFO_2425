229350e47e75a356c967afe0b915158dd6d1e9ab
Pavalean-Alexandru <alexandru.pavalean@istitutoagnelli.it>
2025-01-31 13:35:45 +0100

    aggiunta cartella

---
+++ TPSI/pavalean_alexandru/JS/esercizi_tdd_STUDENTI/esercizio_js_71/soluzione.js
@@ -0,0 +1,27 @@
+export class Poligono {
+   //INSERISCI QUI IL TUO CODICE
+}
+
+export class Quadrato extends Poligono {
+   //INSERISCI QUI IL TUO CODICE
+}
+
+export class TriangoloEquilatero extends Poligono {
+   //INSERISCI QUI IL TUO CODICE
+}
+
+export class Pentagono extends Poligono {
+   //INSERISCI QUI IL TUO CODICE
+}
+
+export class Esagono extends Poligono {
+   //INSERISCI QUI IL TUO CODICE
+}
+
+export class Ettagono extends Poligono {
+   //INSERISCI QUI IL TUO CODICE
+}
+
+export class Ottagono extends Poligono {
+   //INSERISCI QUI IL TUO CODICE
+}
