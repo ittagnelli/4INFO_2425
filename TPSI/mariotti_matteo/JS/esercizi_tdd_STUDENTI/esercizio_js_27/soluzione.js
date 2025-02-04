692ee6bb77d8d20775e53ad366b0499659c3f983
Mariotti-Matteo <matteo.mariotti@istitutoagnelli.it>
2025-02-04 13:18:06 +0100

    Esercizi

--- TPSI/mariotti_matteo/JS/esercizi_tdd_STUDENTI/esercizio_js_27/soluzione.js
+++ TPSI/mariotti_matteo/JS/esercizi_tdd_STUDENTI/esercizio_js_27/soluzione.js
@@ -14,6 +14,6 @@ export const capitalize_all = (str) => {
       }
       c = y.concat(" ")
    }    
-   c = str  
+   c = str
 }
 
--- TPSI/mariotti_matteo/JS/esercizi_tdd_STUDENTI/esercizio_js_28/soluzione.js
+++ TPSI/mariotti_matteo/JS/esercizi_tdd_STUDENTI/esercizio_js_28/soluzione.js
@@ -1,4 +1,17 @@
 export const upper_case = (str) => {
-    //INSERISCI QUI IL TUO CODICE
+    let x = str.split(" ")
+    let y = ""
+
+    for (let i = 0; i < x.length; i++){
+        x[i] = x[i].toUpperCase();
+        y = y.concat(x[i]);
+        if (i < x.length - 1){
+            y = y.concat(" ");
+        }
+        console.log(y);
+    }
+    str = y;
+    console.log(y);
+    return str;
 }
 
