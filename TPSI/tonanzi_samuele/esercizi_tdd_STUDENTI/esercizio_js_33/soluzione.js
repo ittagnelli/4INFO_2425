5c02c56763860f489404dadbee0a58cfbc9f3ade
Tonanzi-Samuele <samuele.github@gmail.com>
2025-02-06 23:42:54 +0100

    riconsegna di tutti gli es svolti dal 23 al 37 (causa problemi con pc)

--- TPSI/tonanzi_samuele/esercizi_tdd_STUDENTI/esercizio_js_33/soluzione.js
+++ TPSI/tonanzi_samuele/esercizi_tdd_STUDENTI/esercizio_js_33/soluzione.js
@@ -1,4 +1,5 @@
 export const concatenateN = (s, n) => {
    //INSERISCI QUI IL TUO CODICE
+   return s.repeat(n)
 };
 
--- TPSI/tonanzi_samuele/esercizi_tdd_STUDENTI/esercizio_js_36/soluzione.js
+++ TPSI/tonanzi_samuele/esercizi_tdd_STUDENTI/esercizio_js_36/soluzione.js
@@ -1,3 +1,5 @@
 export const tronca_parola = (s, pos) => {
    //INSERISCI QUI IL TUO CODICE
+   let list = s.split(' ');
+   return list.slice(0, pos).join(' ');
 };
