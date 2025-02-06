1b8d863210f52fa0b1c4fb1673fbae272ed77b56
Andruetto-Lorenzo <lorenzo.andruetto@istitutoagnelli.it>
2025-02-06 18:36:22 +0100

    consegna es 28-37

--- TPSI/andruetto_lorenzo/JS/esercizi_tdd_STUDENTI/esercizio_js_37/soluzione.js
+++ TPSI/andruetto_lorenzo/JS/esercizi_tdd_STUDENTI/esercizio_js_37/soluzione.js
@@ -1,3 +1,12 @@
 export const rm_string = (s, r) => {
     //INSERISCI QUI IL TUO CODICE
+    let words = s.split(' ');
+    let str = [];
+
+    for(let i = 0; i < words.length; i++){
+        if(words[i] != r)
+            str.push(words[i])
+    }
+
+    return str.join(' ').trim()
 };
