747205511c1d6021ef4a5cfdf67a61997837a014
Trovato-Nickolas <154589467+Trovato-Nickolas@users.noreply.github.com>
2025-02-07 11:26:31 +0100

    Update soluzione.js

--- TPSI/trovato_nickolas/js/esercizi_tdd_STUDENTI/esercizio_js_24/soluzione.js
+++ TPSI/trovato_nickolas/js/esercizi_tdd_STUDENTI/esercizio_js_24/soluzione.js
@@ -1,3 +1,10 @@
 export const protect_email = (email) => {
-   //INSERISCI QUI IL TUO CODICE
+    if (!email ) alert("Riempire campi");
+
+    const [username, domain] = email.split("@");
+    if (!username || !domain) return "";
+
+    return `${username}.@${domain}`;
 };
+const input = "ebosetazew.okoro@istitutoagnelli.it";
+console.log(abbreviazione(input)); 
