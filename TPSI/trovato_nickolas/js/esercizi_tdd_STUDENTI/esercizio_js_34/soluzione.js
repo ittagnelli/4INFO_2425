3efa31a750723c7257c448b4d5428bee03de4b57
Trovato-Nickolas <154589467+Trovato-Nickolas@users.noreply.github.com>
2025-02-07 11:28:11 +0100

    Update soluzione.js

--- TPSI/trovato_nickolas/js/esercizi_tdd_STUDENTI/esercizio_js_34/soluzione.js
+++ TPSI/trovato_nickolas/js/esercizi_tdd_STUDENTI/esercizio_js_34/soluzione.js
@@ -1,3 +1,3 @@
-export const insert = (str1, agg, pos) => {
-    //INSERISCI QUI IL TUO CODICE
+export const insert = (str, insertStr, pos) => {
+    return str.slice(0, pos) + insertStr + str.slice(pos);
 };
