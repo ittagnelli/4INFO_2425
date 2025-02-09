7993db24ad638dd5391168f084c2053833ec8cfb
Andruetto-Lorenzo <lorenzo.andruetto@istitutoagnelli.it>
2025-02-09 12:26:57 +0100

    esercizi js 39-41

--- TPSI/andruetto_lorenzo/JS/esercizi_tdd_STUDENTI/esercizio_js_42/soluzione.js
+++ TPSI/andruetto_lorenzo/JS/esercizi_tdd_STUDENTI/esercizio_js_42/soluzione.js
@@ -1,4 +1,3 @@
 export const swap = (l, n, m) => {
     //INSERISCI QUI IL TUO CODICE
-    l.remove(l[0])
 };
--- TPSI/andruetto_lorenzo/JS/esercizi_tdd_STUDENTI/esercizio_js_43/soluzione.js
+++ TPSI/andruetto_lorenzo/JS/esercizi_tdd_STUDENTI/esercizio_js_43/soluzione.js
@@ -1,4 +1,3 @@
 export const without = (l, n) => {
     //INSERISCI QUI IL TUO CODICE
-    return l.splice(l[n], 1);
 };
