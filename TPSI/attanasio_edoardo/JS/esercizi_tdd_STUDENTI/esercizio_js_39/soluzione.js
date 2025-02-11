81c69dfdeacc247dcb201a8d81ae22963659b208
Attanasio-Edoardo <edoardo.attanasio@istitutoagnelli.it>
2025-02-11 13:50:09 +0100

    esercizi dal 38 al 42

--- TPSI/attanasio_edoardo/JS/esercizi_tdd_STUDENTI/esercizio_js_39/soluzione.js
+++ TPSI/attanasio_edoardo/JS/esercizi_tdd_STUDENTI/esercizio_js_39/soluzione.js
@@ -1,3 +1,12 @@
 export const reverse = (l) => {
     //INSERISCI QUI IL TUO CODICE
-}
+
+    let out = []
+    let sum = 0
+    l.forEach((item, index) => {
+        out.unshift(item)
+        sum += item
+    });
+    out.unshift(sum)
+    return out
+};
--- TPSI/attanasio_edoardo/JS/esercizi_tdd_STUDENTI/esercizio_js_42/soluzione.js
+++ TPSI/attanasio_edoardo/JS/esercizi_tdd_STUDENTI/esercizio_js_42/soluzione.js
@@ -1,3 +1,9 @@
 export const swap = (l, n, m) => {
     //INSERISCI QUI IL TUO CODICE
+    // Normalizza gli indici negativi
+    i = (i % len + len) % len;
+    j = (j % len + len) % len;
+    
+    // Scambia gli elementi
+    [arr[i], arr[j]] = [arr[j], arr[i]];
 };
