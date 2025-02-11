8b2aa1b0540f018a1596fbcec4f087db4e035eca
Andruetto-Lorenzo <lorenzo.andruetto@istitutoagnelli.it>
2025-02-11 12:29:16 +0100

    added ex 42 js

--- TPSI/andruetto_lorenzo/JS/esercizi_tdd_STUDENTI/esercizio_js_42/soluzione.js
+++ TPSI/andruetto_lorenzo/JS/esercizi_tdd_STUDENTI/esercizio_js_42/soluzione.js
@@ -1,3 +1,10 @@
 export const swap = (l, n, m) => {
     //INSERISCI QUI IL TUO CODICE
+    let newArray = [];
+
+    l.forEach((_, i) => {
+        if (l[i] != n) {
+            
+        }
+    });
 };
