976127fe5bf6833d782074f3d1457d9aca77b6b5
Okoro-Wisdom <ebosetazewisdom.okoro@istitutoagnelli.it>
2025-02-11 12:32:53 +0100

    Es 39-40

--- TPSI/okoro_wisdom/JS/esercizi_tdd_STUDENTI/esercizio_js_40/soluzione.js
+++ TPSI/okoro_wisdom/JS/esercizi_tdd_STUDENTI/esercizio_js_40/soluzione.js
@@ -1,4 +1,11 @@
 export const same = (arr1, arr2) => {
-   //INSERISCI QUI IL TUO CODICE
+  if(arr1.length == arr2.length){
+   let uguali = arr1.every((n,i) => n == arr2[i]);
+   return uguali;
+  }
+  else
+  {
+   return false;
+  }
 }
 
