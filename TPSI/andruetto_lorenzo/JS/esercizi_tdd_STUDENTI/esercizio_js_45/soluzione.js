f44b8c58f5ea3303593669cedbf1c864e39dcce2
Andruetto-Lorenzo <lorenzo.andruetto@istitutoagnelli.it>
2025-02-13 21:15:22 +0100

    improved ex 45

--- TPSI/andruetto_lorenzo/JS/esercizi_tdd_STUDENTI/esercizio_js_45/soluzione.js
+++ TPSI/andruetto_lorenzo/JS/esercizi_tdd_STUDENTI/esercizio_js_45/soluzione.js
@@ -1,4 +1,4 @@
 export const only_upper = (s) => {
     //INSERISCI QUI IL TUO CODICE
-    return s.split('').filter(item => item.toUpperCase() === item && item !== ' ' && item !== item.toLowerCase());
+    return s.split('').filter(item => item.charCodeAt(0) >= 65 && item.charCodeAt(0) <= 90);
 };
