1ceef2b18d62b457b29878db04afd11e9e664e6d
Vigano-Luca <154589371+Vigano-Luca@users.noreply.github.com>
2025-02-13 19:22:10 +0100

    modificato l'esercizio

--- TPSI/vigano_luca/JS/esercizi_tdd_STUDENTI/esercizio_js_45/vigano_esercizio_js_45.js
+++ TPSI/vigano_luca/JS/esercizi_tdd_STUDENTI/esercizio_js_45/vigano_esercizio_js_45.js
@@ -1,9 +1,8 @@
 export const only_upper = (s) => {
   let stringa = s.split('');
-  let rimuovere_spazi = stringa.filter((lettera) => lettera === " ");
   let stringa_finale = [];
   for (let i = 0; i < stringa.length; i++) {
-    if (stringa[i] === stringa[i].toUpperCase()) {
+    if (stringa[i] >= 'A' && stringa[i] <= 'Z') {
       stringa_finale.push(stringa[i]);
     }
   }
