8c4135056ac281f9c5c76008a997b19d58436f91
Rossi-Tommaso <tommasoemanuele.rossi@istitutoagnelli.it>
2025-02-25 12:45:00 +0100

    -Esercizi 46 -50

--- TPSI/rossi_tommaso/JS/esercizi_tdd_STUDENTI/esercizio_js_50/soluzione.js
+++ TPSI/rossi_tommaso/JS/esercizi_tdd_STUDENTI/esercizio_js_50/soluzione.js
@@ -1,3 +1,15 @@
 export const order = (l) => {
-   //INSERISCI QUI IL TUO CODICE
-};
+   const firstGroup = l.filter(n => isPrime(parseInt(n)));
+   const secondGroup = l.filter(n => !isPrime(parseInt(n)));
+ 
+   return [...firstGroup, ...secondGroup];
+ };
+
+const isPrime = (n) => {
+   if(n === 1) return true;
+   if(n < 2) return false;
+   for (let i = 2; i <= Math.sqrt(n); i++) {
+     if (n % i === 0) return false;
+   }
+   return true;
+ };
