8d4aee693279c5c8446ab51f70e4fc7c8fb38654
Tonanzi-Samuele <samuele.github@gmail.com>
2025-02-25 12:45:14 +0100

    aggiunta es da 42 a 48

--- TPSI/tonanzi_samuele/esercizi_tdd_STUDENTI/esercizio_js_45/soluzione.js
+++ TPSI/tonanzi_samuele/esercizi_tdd_STUDENTI/esercizio_js_45/soluzione.js
@@ -1,3 +1,8 @@
 export const only_upper = (s) => {
     //INSERISCI QUI IL TUO CODICE
+    
+
+    return s.split("").filter((item)=> item == item.toUpperCase() && item != item.toLowerCase())
+    
+    
 };
--- TPSI/tonanzi_samuele/esercizi_tdd_STUDENTI/esercizio_js_46/soluzione.js
+++ TPSI/tonanzi_samuele/esercizi_tdd_STUDENTI/esercizio_js_46/soluzione.js
@@ -1,4 +1,5 @@
 export const prod_pow2 = (n) => {
    //INSERISCI QUI IL TUO CODICE
+   return n.map((item)=> item**2).reduce((acc,current) => acc * current, 1)
 };
 
