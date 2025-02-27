1186241f3873ff1df6f4c625173c73a68bb96f4b
Andruetto-Lorenzo <lorenzo.andruetto@istitutoagnelli.it>
2025-02-27 20:34:01 +0100

    consegna esercizi da 40 a 58

--- TPSI/andruetto_lorenzo/JS/esercizi_tdd_STUDENTI/esercizio_js_54/soluzione.js
+++ TPSI/andruetto_lorenzo/JS/esercizi_tdd_STUDENTI/esercizio_js_54/soluzione.js
@@ -1,4 +1,5 @@
 export const gen_arr = (len) => {
     //INSERISCI QUI IL TUO CODICE
-    return Math.random()
+    let array = new Array(len).fill(Math.floor(Math.random()))
+    return [array, array.reverse()];
 };
--- TPSI/andruetto_lorenzo/JS/esercizi_tdd_STUDENTI/esercizio_js_57/soluzione.js
+++ TPSI/andruetto_lorenzo/JS/esercizi_tdd_STUDENTI/esercizio_js_57/soluzione.js
@@ -1,5 +1,4 @@
 export const names = (studenti) => {
    //INSERISCI QUI IL TUO CODICE
-   let nomi = new Array;
-   studenti.forEach(object => [...new Array(object.nome)]);
+   return [studenti.map((studente) => studente.nome)];
 }
