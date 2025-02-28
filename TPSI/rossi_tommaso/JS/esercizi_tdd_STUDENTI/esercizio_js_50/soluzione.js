8c7168b44fc303293869984f38d1ba388ccd96c9
Rossi-Tommaso <tommasoemanuele.rossi@istitutoagnelli.it>
2025-02-28 11:12:43 +0100

    esercizi

--- TPSI/rossi_tommaso/JS/esercizi_tdd_STUDENTI/esercizio_js_50/soluzione.js
+++ TPSI/rossi_tommaso/JS/esercizi_tdd_STUDENTI/esercizio_js_50/soluzione.js
@@ -6,10 +6,12 @@ export const order = (l) => {
  };
 
 const isPrime = (n) => {
-   if(n === 1) return true;
-   if(n < 2) return false;
+   if(n === 1 || n === 0)
+     return true;
+   
    for (let i = 2; i <= Math.sqrt(n); i++) {
-     if (n % i === 0) return false;
+     if (n % i === 0) 
+      return false;
    }
    return true;
  };
