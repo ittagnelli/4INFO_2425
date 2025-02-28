65ad448f6703fd200ab603d7923b056334ad6867
Mariotti-Matteo <matteo.mariotti@istitutoagnelli.it>
2025-02-28 11:41:54 +0100

    Fix vari

--- TPSI/mariotti_matteo/JS/esercizi_tdd_STUDENTI/esercizio_js_53/soluzione.js
+++ TPSI/mariotti_matteo/JS/esercizi_tdd_STUDENTI/esercizio_js_53/soluzione.js
@@ -1,5 +1,6 @@
 export const shuffle = (l, m) => {
-   let primopezzo = l.slice(0, m + 1);
-   let secondopezzo = l.slice(m + 1);
-   return primopezzo.concat(secondopezzo);
+   // let primopezzo = l.slice(0, m + 1);
+   // let secondopezzo = l.slice(m + 1);
+   // return primopezzo.concat(secondopezzo);
+   return l.slice(m + 1, ).concat(l.slice(0, m));
 };
