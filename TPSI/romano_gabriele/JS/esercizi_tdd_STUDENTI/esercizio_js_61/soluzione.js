64d2c3d5087987ed1458cb00b9dd8ab922850e15
Romano-Gabriele <gabriele.romano@istitutoagnelli.it>
2025-03-04 13:49:46 +0100

    - aggiunta esercizi 59, 60, 61

--- TPSI/romano_gabriele/JS/esercizi_tdd_STUDENTI/esercizio_js_61/soluzione.js
+++ TPSI/romano_gabriele/JS/esercizi_tdd_STUDENTI/esercizio_js_61/soluzione.js
@@ -1,4 +1,12 @@
-export function count_occurence(text) {
-   //INSERISCI QUI IL TUO CODICE
+function count_occurence(text) {
+  let mappa = new Map();
+  text.split(' ').forEach(word => {
+    let occorrenze = text.split(' ').reduce((acc, current) => word == current ? acc + 1 : acc, 0);
+    text = text.replaceAll(word);
+    mappa.set(word, occorrenze);
+  });
+
+  console.log(mappa);
 }
 
+console.log(count_occurence("JavaScript è un linguaggio molto bello. JavaScript è un linguaggio di alto livello"));
