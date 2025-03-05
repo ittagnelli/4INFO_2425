a9222e2a4a363510515230f2b2683bedce41baab
Andruetto-Lorenzo <lorenzo.andruetto@istitutoagnelli.it>
2025-03-05 18:17:03 +0100

    added some exercises

--- TPSI/andruetto_lorenzo/JS/esercizi_tdd_STUDENTI/esercizio_js_60/soluzione.js
+++ TPSI/andruetto_lorenzo/JS/esercizi_tdd_STUDENTI/esercizio_js_60/soluzione.js
@@ -1,3 +1,5 @@
 export function is_anagram(arr) {
     //INSERISCI QUI IL TUO CODICE
-}  
+    let newArray = new Array();
+    arr.filter(elmnt => elmnt.split('').sort())
+}
--- TPSI/andruetto_lorenzo/JS/esercizi_tdd_STUDENTI/esercizio_js_61/soluzione.js
+++ TPSI/andruetto_lorenzo/JS/esercizi_tdd_STUDENTI/esercizio_js_61/soluzione.js
@@ -1,4 +1,7 @@
 export function count_occurence(text) {
    //INSERISCI QUI IL TUO CODICE
+   let map = new Map();
+   text.split('').forEach(element => map.set(element, 1));
+   return map;
 }
 
