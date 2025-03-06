763134e39f3eb2d8c509dc607dd3ddb3e3a5c040
Macchi-Federico <149145724+Macchi-Federico@users.noreply.github.com>
2025-03-06 16:24:49 +0100

    Consegna esercizi dal 65 al 68

---
+++ TPSI/macchi_federico/JS/macchi_es_66.js
@@ -0,0 +1,11 @@
+function main(){
+
+    let item = ["Egg", 0.25, 12];
+    let [name, quantity, price] = item
+    console.log(`Item: ${name}, Quantity: ${quantity}, Price: ${price}`);
+
+}
+
+
+
+main()
