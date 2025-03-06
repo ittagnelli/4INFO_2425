a506a65bec3f44bd99232e29766fa5c17983d4f1
Rossi-Tommaso <tommasoemanuele.rossi@istitutoagnelli.it>
2025-03-06 22:24:00 +0100

    esercizi js 59-68

--- TPSI/rossi_tommaso/JS/esercizi_tdd_STUDENTI/esercizio_js_62/soluzione.js
+++ TPSI/rossi_tommaso/JS/esercizi_tdd_STUDENTI/esercizio_js_62/soluzione.js
@@ -1,9 +1,9 @@
 export const unique_random_numbers = (n, m) => {
    let set = new Set();
 
-   // while (set.size < n) {
+   while (set.size < n) {
       set.add(Math.trunc(Math.random() * m));
-   // }
+   }
 
    return [...set].sort((a, b) => a - b);
 }
