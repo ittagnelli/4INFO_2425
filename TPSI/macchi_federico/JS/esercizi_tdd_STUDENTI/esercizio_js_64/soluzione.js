307d0c1a0227ce42ceb91dfade1bb1378eebf134
Macchi-Federico <149145724+Macchi-Federico@users.noreply.github.com>
2025-03-06 16:28:12 +0100

    Update soluzione.js

--- TPSI/macchi_federico/JS/esercizi_tdd_STUDENTI/esercizio_js_64/soluzione.js
+++ TPSI/macchi_federico/JS/esercizi_tdd_STUDENTI/esercizio_js_64/soluzione.js
@@ -1,4 +1,17 @@
 export const count_vocals = (str) => {
  //INSERISCI QUI IL TUO CODICE
+    let mappa = new Map();
+    let vocali = ['a', 'e', 'i', 'o', 'u'];
+
+    str.split('').forEach(carattere => {
+        if(vocali.includes(carattere) ){
+
+            mappa.set(carattere, (mappa.get(carattere)|| 0) + 1);             
+
+
+        }       
+    });    
+
+    return mappa
 }
 
