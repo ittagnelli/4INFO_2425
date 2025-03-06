b55906fde34b208124c964fbdc3492c80aa0e420
Vigano-Luca <154589371+Vigano-Luca@users.noreply.github.com>
2025-03-06 19:08:14 +0100

    messo l'esercizio 61

---
+++ TPSI/vigano_luca/JS/esercizi_tdd_STUDENTI/esercizio_js_61/es_61.js
@@ -0,0 +1,11 @@
+export function count_occurence(text) {
+   const words = text.split(/\s+/);
+    const wordCount = new Map();
+
+    words.forEach(word => {
+        wordCount.set(word, (wordCount.get(word) || 0) + 1);
+    });
+
+    return wordCount;
+}
+
