8139c3659af64f77b239f394751de911dd77eb26
Andruetto-Lorenzo <lorenzo.andruetto@istitutoagnelli.it>
2025-03-10 09:18:33 +0100

    consegna es 69,70

--- TPSI/andruetto_lorenzo/JS/esercizi_tdd_STUDENTI/esercizio_js_70/soluzione.js
+++ TPSI/andruetto_lorenzo/JS/esercizi_tdd_STUDENTI/esercizio_js_70/soluzione.js
@@ -1,3 +1,11 @@
 export class Calcolatrice {
- //INSERISCI QUI IL TUO CODICE
+    //INSERISCI QUI IL TUO CODICE
+    constructor (ris) { let set_result = ris }
+    
+    set set_result(ris) { this.result = ris }
+    get get_result() { return this.result }
+    sum (num) { return this.result + num }
+    sub (num) { return this.result - num }
+    mul (num) { return this.result * num }
+    div (num) { return this.result / num }
 }
