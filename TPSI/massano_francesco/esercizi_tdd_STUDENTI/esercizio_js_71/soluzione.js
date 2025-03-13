b959bea1ea675210c732c355159dd00baeb11cd5
Massano-Francesco <massano.francesco@istitutoagnelli.it>
2025-03-13 20:02:51 +0100

    consegna esercizi

--- TPSI/massano_francesco/esercizi_tdd_STUDENTI/esercizio_js_71/soluzione.js
+++ TPSI/massano_francesco/esercizi_tdd_STUDENTI/esercizio_js_71/soluzione.js
@@ -1,27 +1,54 @@
 export class Poligono {
-   //INSERISCI QUI IL TUO CODICE
+   constructor(n1,n2){
+      this.numLati = n1;
+      this.long = n2;
+   }
+
+   perimetro(){
+      return this.numLati * this.long;
+   }
+
+   area(){
+      return (this.perimetro * a)/2;
+   }
 }
 
 export class Quadrato extends Poligono {
-   //INSERISCI QUI IL TUO CODICE
+   constructor(n2){
+      super(4,n2);
+   }
+   area(){
+      return this.long **2;
+   }
+   
 }
 
 export class TriangoloEquilatero extends Poligono {
-   //INSERISCI QUI IL TUO CODICE
+   constructor(n1,n2){
+      super(n1,n2);
+   }
 }
 
 export class Pentagono extends Poligono {
-   //INSERISCI QUI IL TUO CODICE
+   constructor(n1,n2){
+      super(n1,n2);
+   }
 }
 
 export class Esagono extends Poligono {
-   //INSERISCI QUI IL TUO CODICE
+   constructor(n1,n2){
+      super(n1,n2);
+   }
 }
 
 export class Ettagono extends Poligono {
-   //INSERISCI QUI IL TUO CODICE
+   constructor(n1,n2){
+      super(n1,n2);
+   }
 }
 
 export class Ottagono extends Poligono {
-   //INSERISCI QUI IL TUO CODICE
+   constructor(n1,n2){
+      super(n1,n2);
+   }
 }
