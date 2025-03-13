4fdd6df273c72fbe376f786b535e77da10843830
Macchi-Federico <federicomaurilio.macchi@istitutoagnelli.it>
2025-03-13 11:14:33 +0100

    EX 70

--- TPSI/macchi_federico/JS/esercizi_tdd_STUDENTI/esercizio_js_70/soluzione.js
+++ TPSI/macchi_federico/JS/esercizi_tdd_STUDENTI/esercizio_js_70/soluzione.js
@@ -1,13 +1,34 @@
 export class Calcolatrice {
-
-    somma(){
-
-
+    constructor() {
+        this.tot = 0;
     }
 
-    differenza(){
-        
+    set_result(n) {
+        this.tot = n;
+        return this; 
     }
 
-    
+    sum(n) {
+        this.tot += n;
+        return this;
+    }
+
+    sub(n) {
+        this.tot -= n;
+        return this;
+    }
+
+    mul(n) {
+        this.tot *= n;
+        return this;
+    }
+
+    div(n) {
+        this.tot /= n;
+        return this;
+    }
+
+    get_result() {
+        return this.tot;
+    }
 }
