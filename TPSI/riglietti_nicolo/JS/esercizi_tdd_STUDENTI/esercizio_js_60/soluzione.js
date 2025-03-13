85166399eeb8cc6d2f26874a6ed252e99202eb1f
Riglietti-Nicolo <riglietti.nicolo@istitutoagnelli.it>
2025-03-13 22:35:48 +0100

    consgena esercizi tpsi

--- TPSI/riglietti_nicolo/JS/esercizi_tdd_STUDENTI/esercizio_js_60/soluzione.js
+++ TPSI/riglietti_nicolo/JS/esercizi_tdd_STUDENTI/esercizio_js_60/soluzione.js
@@ -1,11 +1,11 @@
 export function is_anagram(arr) {
-    const parole = arr.reduce((acc, parola) => {
+    const gruppiParole = arr.reduce((acc, parola) => {
         const chiave = [...parola.toLowerCase()].sort().join('');
-        acc.set(chiave, (acc.get(chiave) || []).concat(parola));
+        acc.set(chiave, parola); (acc.get(chiave) || []).concat(parola)
         return acc;
     }, new Map());
-
-    return Array.from(new Set(arr.map(parola =>  parole.get([...parola.toLowerCase()].sort().join('')).length > 1 ? parole.get([...parola.toLowerCase()].sort().join('')).at(-1) : parola)));
+    
+    return [...gruppiParole.values()]
 }  
 
 // let map = new Map
