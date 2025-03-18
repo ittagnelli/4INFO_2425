3e428ba9a19c15f94dbba2e6592c130f1376bb3c
Mariotti-Matteo <matteo.mariotti@istitutoagnelli.it>
2025-03-18 12:18:26 +0100

    Esercizi

--- TPSI/mariotti_matteo/JS/esercizi_tdd_STUDENTI/esercizio_js_61/soluzione.js
+++ TPSI/mariotti_matteo/JS/esercizi_tdd_STUDENTI/esercizio_js_61/soluzione.js
@@ -1,16 +1,28 @@
+// export function count_occurence(text) {
+//    const words = text.toLowerCase().split(' ');
+//    const wordMap = new Map();
+
+//    if (words) {
+//        words.forEach(word => {
+//            if (wordMap.has(word)) {
+//                wordMap.set(word, wordMap.get(word) + 1);
+//            } else {
+//                wordMap.set(word, 1);
+//            }
+//        });
+//    }
+
+//    return wordMap;
+// }
+
+
 export function count_occurence(text) {
-   const words = text.toLowerCase().split(' ');
-   const wordMap = new Map();
+    let atext = text.split(' ');
+    let occorrenze = new Map();
 
-   if (words) {
-       words.forEach(word => {
-           if (wordMap.has(word)) {
-               wordMap.set(word, wordMap.get(word) + 1);
-           } else {
-               wordMap.set(word, 1);
-           }
-       });
-   }
+    atext.forEach(word => {
+        occorrenze.set(word, (occorrenze.get(word) || 0) + 1);
+    });
 
-   return wordMap;
+return occorrenze;
 }
--- TPSI/mariotti_matteo/JS/esercizi_tdd_STUDENTI/esercizio_js_72/soluzione.js
+++ TPSI/mariotti_matteo/JS/esercizi_tdd_STUDENTI/esercizio_js_72/soluzione.js
@@ -14,5 +14,9 @@ export class Docente {
 }
 
 export const confrontaOBJ = (obj1, obj2) => {
-  return obj1.nome === obj2.nome && obj1.eta === obj2.eta;
+   // return obj1.nome === obj2.nome && obj1.eta === obj2.eta;
+
+  return (
+    ((obj1 instanceof Studente) && (obj2 instanceof Studente)) || ((obj1 instanceof Docente) && (obj2 instanceof Docente))
+  ) && JSON.stringify(obj1) === JSON.stringify(obj2);
 };
