facf226d9165ba43246ce8f13c0a1db86d974cb2
Riglietti-Nicolo <riglietti.nicolo@istitutoagnelli.it>
2025-03-18 12:25:01 +0100

    es dal 74 al 78

--- TPSI/riglietti_nicolo/JS/esercizi_tdd_STUDENTI/esercizio_js_74/soluzione.js
+++ TPSI/riglietti_nicolo/JS/esercizi_tdd_STUDENTI/esercizio_js_74/soluzione.js
@@ -1,3 +1,21 @@
 export class Automobile {
-    //INSERISCI QUI IL TUO CODICE
+    constructor(brand, model, year, color, cc){
+        this.brand = brand;
+        this.model = model;
+        this.year = year;
+        this.color = color;
+        this.cc = cc;
+    }
+
+    accensione(){
+        console.log('accesa');
+    }
+
+    spegnimento(){
+        console.log('spenta');
+    }
+
+    caratteristiche() {
+        console.log(`brand: ${this.brand} modello: ${this.model} anno: ${this.year} colore: ${this.color} cc: ${this.cc}`)
+    }
 }
--- TPSI/riglietti_nicolo/JS/esercizi_tdd_STUDENTI/esercizio_js_78/soluzione.js
+++ TPSI/riglietti_nicolo/JS/esercizi_tdd_STUDENTI/esercizio_js_78/soluzione.js
@@ -1,3 +1,18 @@
 export class Orario {
-   //INSERISCI QUI IL TUO CODICE
+   constructor(h, m, s) {
+      this.h = h
+      this.m = m
+      this.s = s
+   }
+
+   millisecondi() { return (this.h * 3600 + this.m * 60 + this.s) * 1000; }
+
+   min(orario2, orario3) {
+
+      let ms1 = this.millisecondi()
+      let ms2 = orario2.millisecondi()
+      let ms3 = orario3.millisecondi()
+
+      return ms1 < ms2 &&ms1 < ms3 ? {h: this.h, m: this.m, s: this.s } : ms2 < ms1 && ms2 < ms3 ? {h: orario2.h, m: orario2.m, s: orario2.s} : {h: orario3.h, m: orario3.m, s: orario3.s}
+   }
 }
