f7bb5912db6e12b133ace58238248c2cf2ffa0de
Mariotti-Matteo <matteo.mariotti@istitutoagnelli.it>
2025-03-20 17:21:45 +0100

    Es 74 - 79

--- TPSI/mariotti_matteo/JS/esercizi_tdd_STUDENTI/esercizio_js_77/soluzione.js
+++ TPSI/mariotti_matteo/JS/esercizi_tdd_STUDENTI/esercizio_js_77/soluzione.js
@@ -1,3 +1,24 @@
 export class Fibonacci {
-    //INSERISCI QUI IL TUO CODICE
+    constructor(n){
+        this.n = n;
+    }
+    
+    calc(){
+        let a = 0, b = 1, c = 0
+        if(this.n >= 2){
+            for(let i = 0; i < this.n; i++){
+                c = a + b;
+                a = b;
+                b = c;
+            }
+        }
+        else if(this.n == 1){
+            a = 1;
+        }
+        else if(this.n == 0){
+            a = 0;
+        }
+        return a;
+    }
+    
 }
