b294533b39c030005e3e9749ff707dcadb63275e
Attanasio-Edoardo <edoardo.attanasio@istitutoagnelli.it>
2025-03-20 21:57:42 +0100

    consegna esercizi 79

--- TPSI/attanasio_edoardo/JS/esercizi_tdd_STUDENTI/esercizio_js_75/soluzione.js
+++ TPSI/attanasio_edoardo/JS/esercizi_tdd_STUDENTI/esercizio_js_75/soluzione.js
@@ -1,15 +1,56 @@
 class Enumera {
-    //INSERISCI QUI IL TUO CODICE
+    enumera() {
+        return Object.keys(this);
+    }
 }
 
 export class Animale extends Enumera {
-    //INSERISCI QUI IL TUO CODICE
+    constructor(specie, nome, eta) {
+        super();
+        this.specie = specie;
+        this.nome = nome;
+        this.eta = eta;
+    }
+
+    descrizione() {
+        return `${this.nome} è una ${this.specie} di ${this.eta} anni.`;
+    }
+
+    verso() {
+        return `${this.specie} fa un verso caratteristico.`;
+    }
 }
 
 export class Automobile extends Enumera {
-   //INSERISCI QUI IL TUO CODICE
+    constructor(colore, marca, modello) {
+        super();
+        this.colore = colore;
+        this.marca = marca;
+        this.modello = modello;
+    }
+
+    accendi() {
+        return `${this.marca} ${this.modello} è stata accesa.`;
+    }
+
+    spegni() {
+        return `${this.marca} ${this.modello} è stata spenta.`;
+    }
 }
 
 export class Poligono extends Enumera {
-   //INSERISCI QUI IL TUO CODICE
+    constructor(lato, n_lati, reg) {
+        super();
+        this.lato = lato;
+        this.n_lati = n_lati;
+        this.reg = reg;
+    }
+
+    perimetro() {
+        return this.n_lati * this.lato;
+    }
+
+    area() {
+        return this.reg ? (this.n_lati * this.lato ** 2) / (4 * Math.tan(Math.PI / this.n_lati)) : null;
+    }
 }
