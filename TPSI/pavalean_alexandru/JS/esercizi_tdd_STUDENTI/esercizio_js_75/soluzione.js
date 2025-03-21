1fcc4fdbf8903d702a31711502157e5e70a53f80
VasileTimis <vasile.timis@istitutoagnelli.it>
2025-03-21 08:32:39 +0100

    consegna esercizi dal 74 al 79

--- TPSI/pavalean_alexandru/JS/esercizi_tdd_STUDENTI/esercizio_js_75/soluzione.js
+++ TPSI/pavalean_alexandru/JS/esercizi_tdd_STUDENTI/esercizio_js_75/soluzione.js
@@ -1,15 +1,33 @@
 class Enumera {
     //INSERISCI QUI IL TUO CODICE
+    constructor(a, b, c){
+        this.a = a
+        this.b = b
+        this.c = c
+    }
+
+    enumera(){
+        return [this.a, this.b, this.c]
+    }
 }
 
 export class Animale extends Enumera {
     //INSERISCI QUI IL TUO CODICE
+    constructor(specie, nome, eta){
+        super('specie', 'nome', 'eta')
+    }
 }
 
 export class Automobile extends Enumera {
    //INSERISCI QUI IL TUO CODICE
+    constructor(colore, marca, modello){
+        super('colore', 'marca', 'modello')
+    }
 }
 
 export class Poligono extends Enumera {
    //INSERISCI QUI IL TUO CODICE
+    constructor(lato, n_lati, reg){
+        super('lato', 'n_lati', 'reg')
+    }
 }
