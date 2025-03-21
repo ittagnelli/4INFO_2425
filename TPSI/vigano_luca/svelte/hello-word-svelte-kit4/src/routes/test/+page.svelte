38599a120b9955d9086703c7cf16f0766c620cf4
Vigano-Luca <154589371+Vigano-Luca@users.noreply.github.com>
2025-03-21 13:44:53 +0100

    aggiunto esercizio 2-3-4

---
+++ TPSI/vigano_luca/svelte/hello-word-svelte-kit4/src/routes/test/+page.svelte
@@ -0,0 +1,14 @@
+<!-- js -->
+<script>
+    let name = "Jonh Doe";
+</script>
+
+<!-- HTML -->
+<h1> My name is {name}!</h1>
+
+<!-- css -->
+ <style>
+    h1 {
+        color: red;
+    }
+ </style>
