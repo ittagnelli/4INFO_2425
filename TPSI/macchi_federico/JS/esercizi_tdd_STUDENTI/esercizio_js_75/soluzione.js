a8fcd07b70d44dffc875ac8190f5b244dc3f9d07
Macchi-Federico <149145724+Macchi-Federico@users.noreply.github.com>
2025-03-21 08:24:06 +0100

    Update soluzione.js

--- TPSI/macchi_federico/JS/esercizi_tdd_STUDENTI/esercizio_js_75/soluzione.js
+++ TPSI/macchi_federico/JS/esercizi_tdd_STUDENTI/esercizio_js_75/soluzione.js
@@ -1,15 +1,40 @@
 class Enumera {
     //INSERISCI QUI IL TUO CODICE
+    constructor(x, y, z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+}
+ stampa() {
+    return [this.x, this.y, this.z];
+}
+
 }
 
 export class Animale extends Enumera {
     //INSERISCI QUI IL TUO CODICE
+
+    constructor(razza, colore, nome) {
+        super(razza, colore, nome);
+    }
+
+
 }
 
 export class Automobile extends Enumera {
    //INSERISCI QUI IL TUO CODICE
+
+    constructor(marca, anno, modello) {
+        super(marca, anno, modello);
+    }
+
 }
 
 export class Poligono extends Enumera {
    //INSERISCI QUI IL TUO CODICE
+
+    constructor(l1, l2, forma) {
+        super(l1, l2, forma);
+    }
+
 }
