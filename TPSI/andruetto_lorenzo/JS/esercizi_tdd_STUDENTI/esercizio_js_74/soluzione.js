ef193bc31d41f5db096c0831806504c79a427465
Andruetto-Lorenzo <lorenzo.andruetto@istitutoagnelli.it>
2025-03-21 09:50:40 +0100

    consegna es 74,75,76 js

--- TPSI/andruetto_lorenzo/JS/esercizi_tdd_STUDENTI/esercizio_js_74/soluzione.js
+++ TPSI/andruetto_lorenzo/JS/esercizi_tdd_STUDENTI/esercizio_js_74/soluzione.js
@@ -2,5 +2,7 @@ export class Automobile {
     //INSERISCI QUI IL TUO CODICE
     constructor() {}
 
-    accendi() { console.log(`L'auto è accesa`);  }
+    accendi() { console.log(`L'auto è accesa.`);  };
+    spegni() { console.log(`L'auto è spenta.`);  };
+    benzina() { console.log(`L'auto ha fatto il pieno.`);  };
 }
--- TPSI/andruetto_lorenzo/JS/esercizi_tdd_STUDENTI/esercizio_js_76/soluzione.js
+++ TPSI/andruetto_lorenzo/JS/esercizi_tdd_STUDENTI/esercizio_js_76/soluzione.js
@@ -1,3 +1,17 @@
 export class Orario {
     //INSERISCI QUI IL TUO CODICE
+    constructor(ore, minuti, secondi) {this.h = ore; this.m = minuti; this.s = secondi;};
+
+    diff(orario) {
+        this.milli = (this.h * 3600 + this.m * 60 + this.s) * 1000;
+        let msOrario = (orario.h * 3600 + orario.m * 60 + orario.s) * 1000;
+        const diff = this.milli > msOrario ? this.milli - msOrario : msOrario - this.milli;
+
+        return {
+            milliseconds: diff,
+            seconds: diff / 1000,
+            minutes: diff / 60000,
+            hours: diff / 3600000,
+        }
+    }
 }
